6576d8f05004e2c22c2d1a7414ce57f518097beb
Hy Huynh <hyxhuynh@gmail.com>
2020-10-26 12:20:01 -0700

    Initial commit

---
+++ NewConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine(name + " is " + age + " years old. Is it correct? (Y/N)");
+            var confirmation = Console.ReadLine().ToLower();
+
+            while (confirmation == "n")
+            {
+                Console.WriteLine("How old are you?");
+                age = Console.ReadLine();
+                Console.WriteLine(name + " is " + age + " years old. Is it correct? (Y/N)");
+                confirmation = Console.ReadLine().ToLower();
+            }
+
+            if (confirmation == "y")
+            {
+                Console.WriteLine(name + " is " + age + " years old.");
+                Console.WriteLine("Thank you for inputing your information!");
+            }
+        }
+    }
+}
